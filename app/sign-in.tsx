48902b78431f54229e8b19255deb9b6af7b2c8af
abdulRehman7071 <karzbaba47@gmail.com>
2024-12-26 01:20:18 +0530

    App Name

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -27,7 +27,7 @@ const SignIn = () => {
     <Image source={images.onboarding} className=' w-full h-4/6' resizeMode='contain' /> 
     <View className='px-10'>
       <Text className=' text-base text-center uppercase font-rubik text-black-200 '>
-        Welcome to ReState
+        Welcome to DreamDwell
       </Text>
       <Text className=' text-3xl font-rubik-bold text-black-300 text-center mt-2'>
         Let's Get You Closer to {"\n"}
@@ -36,7 +36,7 @@ const SignIn = () => {
         </Text>
       </Text>
       <Text className=' text-lg font-rubik text-black-200 text-center mt-12'>
-          Login to Restate with Google
+          Login to DreamDwell with Google
         </Text>
 
         <TouchableOpacity onPress={handleLogin} className=' bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'>
